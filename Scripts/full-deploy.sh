f45a63965e2a5e25a23c8de3a3fedd6d0c9cbfcd
Citronik <perdoch.filip@gmail.com>
2024-01-22 19:19:29 +0100

    full-deploy dependency, debug

--- Scripts/full-deploy.sh
+++ Scripts/full-deploy.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 MODEL_NAME="test"
-SCRIPT_BASE_PATH="/home/student-test"
-HELP_VARIOATIONS="^-?-?h(elp)?$"
-CHARMS_FILE="/home/$MAAS_LOGIN/bundleKISprod-cephWEB.yaml"
+USER=$(whoami)
+SCRIPT_BASE_PATH=$(pwd)
+CHARMS_FILE="$SCRIPT_BASE_PATH/bundleKISprod-cephWEB.yaml"
 MAAS_LOGIN="student"
-MAAS_API_KEY="/home/$MAAS_LOGIN/maas-api-key"
+MAAS_API_KEY="$SCRIPT_BASE_PATH/maas-api-key"
 MAAS_URL="http://10.11.0.2:5240/MAAS"
 
 
+debug_print() {
+	echo $MODEL_NAME
+	echo $USER
+	echo $SCRIPT_BASE_PATH
+	echo $CHARMS_FILE
+	echo $MAAS_LOGIN
+	echo $MAAS_API_KEY
+	echo $MAAS_URL
+}
+
 print_help() {
-	echo "      full-deploy.sh [MODEL_NAME] [CHARMS_FILE] [OPTIONS]"
-    echo "[---------------------------------------------]"
-    echo "necessary arguments:"
-	echo "      MODEL_NAME	        name for the model where openstack will be deployed"
+	echo "      full-deploy.sh [CHARMS_FILE] [OPTIONS]"
+	echo "[---------------------------------------------]"
+	echo "necessary arguments:"
 	echo "      CHARMS_FILE	        path to the openstack charms.yaml file"
-    echo "[---------------------------------------------]"
-    echo "optional arguments:"
-    echo "      --maas-login	    loggin to maas as a user"
-    echo "      --maas-url	        maas url"
-    echo "      --maas-api-key	    maas api key"
+	echo "[---------------------------------------------]"
+	echo "optional arguments:"
+	echo "      --model-name	    name of the model to be created"
+	echo "      --maas-login	    loggin to maas as a user"
+	echo "      --maas-url	        maas url"
+	echo "      --maas-api-key	    maas api key"
 	echo "      --help -h		    print this help message"
-    echo "      --version -v	    print the version of this script"
-    echo "[---------------------------------------------]"
+	echo "      --version -v	    print the version of this script"
+	echo "[---------------------------------------------]"
 }
 
 check_For_Other() {
@@ -37,46 +47,80 @@ check_For_Other() {
     fi
 }
 
-login_To_Maas() {
-    maas login $MAAS_LOGIN $MAAS_URL - < /home/student-test/maas-api-key
-}
 check_For_Dependencies() {
+	echo "Checking for dependencies..."
+	if [ "$#" -ne 1 ]; then
+		echo "Usage: $0 <charms.yaml>"
+		exit 1
+	fi
 	if ! command -v juju &> /dev/null; then
 		echo "Juju is not installed. Please install Juju before running this script."
 		exit 1
 	fi
-
-	# Check if MAAS is available
 	maas_status=$(maas status 2>&1 || true)
 	if [[ $maas_status == *"command not found"* ]]; then
 		echo "MAAS command not found. Please install MAAS before running this script."
 		exit 1
 	fi
+	echo "Dependency check completed succesfully! :)"
+}
 
-    if [ "$#" -ne 1 ]; then
-        echo "Usage: $0 <MODEL_NAME>"
-        exit 1
-    fi
+parse_attributes() {
+	while [ $# -gt 0 ]; do
+		case "$2" in
+			--maas-login)
+				MAAS_LOGIN="$3"
+				shift 2
+				;;
+			--maas-url)
+				MAAS_URL="$3"
+				shift 2
+				;;
+			--maas-api-key)
+				MAAS_API_KEY="$3"
+				shift 2
+				;;
+			--help|-h)
+				print_help
+				exit 0
+				;;
+			--version|-v)
+				echo "Full OpenStack deploy version 1.0"
+				exit 0
+				;;
+			*)
+				echo "Unknown option: $1"
+				exit 1
+				;;
+		esac
+	done
+}
 
-
-	# Check if charms.yaml file is provided as an argument
-	if [ "$#" -ne 2 ]; then
-		echo "Usage: $0 <charms.yaml>"
-		exit 1
-	fi
+login_To_Maas() {
+    maas login $MAAS_LOGIN $MAAS_URL - < $MAAS_API_KEY
 }
 
 deploy_The_Charms() {
 	maas $MAAS_LOGIN discoveries clear all=True
-
-
 	juju add-model $MODEL_NAME
-	juju grant test-student admin $MODEL_NAME
+	juju grant $ admin $MODEL_NAME
 	juju deploy $SCRIPT_BASE_PATH/bundleKISprod-cephWEB.yaml
 }
+
+echo "Starting OpenStack deployment..."
+
 check_For_Other $@
 
 check_For_Dependencies $@
+
+debug_print
+
+parse_attributes $@
+
+debug_print
+#login_To_Maas $@
+
+
 
 echo "OpenStack deployment completed succesfully! :)"
 exit 0
